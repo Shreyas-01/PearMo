4b9827718a332d6172e296e796ea592d1e790459
Shreyas <shreyassomvanshi@gmail.com>
2021-09-04 17:46:43 +0530

    Update FeedRoute.js

--- Backend/API/Routes/FeedRoute.js
+++ Backend/API/Routes/FeedRoute.js
@@ -1,60 +1,55 @@
 const express = require('express');
 const Router = express.Router();
 const Post = require('../Models/PostSchema');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-Router.get('/', (req,res,next) => {
-    const filter = req.query.filter;
+Router.get('/trending', (req,res,next) => {
+    Post.find().sort([['date', -1]]).limit(20).sort([['likes', 1]])
+    .then(posts => {
+        const count = posts.length;
+        var random = Math.floor(Math.random() * count)
 
-    if(filter === 'recent'){
-        Post.find().sort([['date', 1]]).limit(20)
-        .then(posts => {
-            console.log("Posts fetched successfully")
-            res.status(201).json({
-                posts
+        try{
+            console.log("Post fetched successfully")
+            res.status(401).json({
+                post: posts[random]
             });
-        })
-        .catch((error) => {
+        }
+        catch{error =>{
             console.log("Could not fetch posts")
             res.status(401).json({
                 message: 'Could not fetch posts',
                 error: error.message
             });
-        });
-    }
-    else if(filter === 'trending'){
-        Post.find().sort([['date', 1]]).limit(20)
-        .then(posts => {
-            const count = posts.length;
-            var random = Math.floor(Math.random() * count)
-
-            try{
-                console.log("Post fetched successfully")
-                res.status(401).json({
-                    post: posts[random]
-                });
-            }
-            catch{error =>{
-                console.log("Could not fetch posts")
-                res.status(401).json({
-                    message: 'Could not fetch posts',
-                    error: error.message
-                });
-            }}
-        })
-        .catch((error) => {
-            console.log("Could not fetch posts")
-            res.status(401).json({
-                message: 'Could not fetch posts',
-                error: error.message
-            });
-        });
-    }
-    else{
+        }}
+    })
+    .catch((error) => {
+        console.log("Could not fetch trending posts")
         res.status(401).json({
-            message: 'Invalid post fetching request',
+            message: 'Could not fetch trending posts',
+            error: error.message
         });
-    }
+    });
 });
 
+
+Router.get('/recent/:loadmore', (req,res,next) => {
+
+    const nextposts = req.params.loadmore * 1;
+
+    Post.find().sort([['date', -1]]).skip(nextposts).limit(1)
+    .then(posts => {
+        console.log("Posts fetched successfully")
+        res.status(201).json({
+            posts
+        });
+    })
+    .catch((error) => {
+        console.log("Could not fetch recent posts")
+        res.status(401).json({
+            message: 'Could not fetch recent posts',
+            error: error.message
+        });
+    });
+});
 module.exports = Router;
